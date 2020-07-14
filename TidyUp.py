ce8b04d3c4bcc9f22594d9918060dde3bc53a821
Jesse Appiah <TheRealJenius@gmail.com>
2020-07-14 23:43:34 +0100

    Creating the program file that will tidy up the device

---
+++ TidyUp.py
@@ -0,0 +1,6 @@
+'''
+The aim of this program is to:
+- Confirm duplicates within a device
+- Provide the user with the option of which duplicates to delete or move
+- Empty the recyling bin (as a bonus)
+'''
