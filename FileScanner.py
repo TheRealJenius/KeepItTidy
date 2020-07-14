0dfa9974c0da5905f32ffc43c1a769e66fd368f7
Jesse Appiah <TheRealJenius@gmail.com>
2020-07-14 23:40:03 +0100

    Creating the File Scanner's program file

---
+++ FileScanner.py
@@ -0,0 +1,6 @@
+'''
+The purposes of this program is to:
+- Confirm the files and folders on a chosen drive
+- Export the values to an excel file
+- Link to another program to keept things tidy
+'''
